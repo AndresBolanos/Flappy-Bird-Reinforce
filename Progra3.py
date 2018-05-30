1a79c9f051208af2a1c00d7a20ccc0e7e95f8052
AndresBolanos <ronald-9797@hotmail.com>
2018-05-29 21:44:19 -0700

    Guarda pesos

--- Progra3.py
+++ Progra3.py
@@ -43,7 +43,7 @@ def close_borders(img):
 def resize_Image(img):
     height = img.shape[0]
     width = img.shape[1]
-    res = cv.resize(img,(200, 200), interpolation = cv.INTER_NEAREST)
+    res = cv.resize(img,(100, 100), interpolation = cv.INTER_NEAREST)
     return res
 
 #Exceute all the process to convert the image
@@ -57,23 +57,169 @@ def convert_image(img):
     black_white_image = resize_Image(black_white_image)
     return black_white_image
 
+
+################################################################
+#Ejemplos
+#https://github.com/pytorch/examples/blob/master/reinforcement_learning/actor_critic.py
+#https://github.com/pytorch/examples/blob/master/mnist/main.py
+#https://github.com/pytorch/examples
+
+#Red
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Categorical
+from collections import namedtuple
+import torch.optim as optim
+from pathlib import Path
+
+parser = argparse.ArgumentParser(description='PyTorch actor-critic example')
+parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
+                    help='discount factor (default: 0.99)')
+parser.add_argument('--seed', type=int, default=543, metavar='N',
+                    help='random seed (default: 1)')
+parser.add_argument('--render', action='store_true',
+                    help='render the environment')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='interval between training status logs (default: 10)')
+args = parser.parse_args()
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+        #Se crean tres convoluciones
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)                #Entradas(1 imagen), #Kernels, Size_kernel / Convolucion para train
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)               #Entradas, #Kernels, Size_kernel
+        
+        self.conv2_drop = nn.Dropout2d()
+
+        #Salida de la convolucion, tamano del layer
+        #self.fc1 = nn.Linear( 9680, 2)                                # Se disminuye el tamano de la capa
+        self.fc2 = nn.Linear(9680, 2)                                       #Solo dos salidas, arriba o abajo
+        self.fc3 = nn.Linear(9680, 1)                                       #Solo dos salidas, arriba o abajo
+
+        #Guardar informacion
+        self.saved_actions = []
+        self.rewards = []
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+
+        #Hace otro reshape a (#imagenes, reduccion del vector)
+        x = x.view(-1, x.shape[1]*x.shape[2]*x.shape[3])
+
+        #Funcion de activacion
+        #x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        state_values = self.fc3(x)
+        action_scores = self.fc2(x)
+        
+        return F.softmax(action_scores, dim=1), state_values[0]
+
+model = Net()              #Instancia la red
+
+#Estructura de la accion guardada
+SavedAction = namedtuple('SavedAction', ['log_prob', 'value'])
+eps = np.finfo(np.float32).eps.item()
+optimizer = optim.Adam(model.parameters(), lr=3e-2)
+
+def select_action(model, state):
+    #Se convierte la imagen a arreglo de thorch
+    state = torch.from_numpy(state)
+    #Hace reshape para garantizar un canal de entrada
+    #Canales, #Imagenes, ancho, alto
+    state= state.reshape(1, 1, state.shape[0], state.shape[1])
+    #Convierte los valores a float, asi lo recibe conv2d
+    state = state.type(torch.FloatTensor)
+    probs, state_value = model(state)
+    m = Categorical(probs)
+    action = m.sample()
+    model.saved_actions.append(SavedAction(m.log_prob(action), state_value))
+    return action.item()
+
+def finish_episode():
+    R = 0
+    saved_actions = model.saved_actions
+    policy_losses = []
+    value_losses = []
+    rewards = []
+    for r in model.rewards[::-1]:
+        R = r + args.gamma * R
+        rewards.insert(0, R)
+    rewards = torch.tensor(rewards)
+    rewards = (rewards - rewards.mean()) / (rewards.std() + eps)
+    for (log_prob, value), r in zip(saved_actions, rewards):
+        reward = r - value.item()
+        policy_losses.append(-log_prob * reward)
+        value_losses.append(F.smooth_l1_loss(value, torch.tensor([r])))
+    loss = torch.stack(policy_losses).sum() + torch.stack(value_losses).sum()
+    loss.backward()
+    #Se guarda el estado de la red
+    torch.save(model.state_dict(), "Pesos.txt")
+    optimizer.step()
+    optimizer.zero_grad()
+    del model.rewards[:]
+    del model.saved_actions[:]
+    
 def run():
+    # Se cargan los pesos
+    file = Path("Pesos.txt")
+    if (file.exists()):
+        model.load_state_dict(torch.load("Pesos.txt"))
+        model.eval()
+
+    #Se instancia el juego 
     game = FlappyBird()
-    p = PLE(game, fps=30, display_screen=True)
-    #agent = myAgentHere(allowed_actions=p.getActionSet())
+    p = PLE(game, fps=30, display_screen=True, reward_values={"loss": -1.0, "win": 1.0})
 
     p.init()
     reward = 0.0
+    running_reward = 10
+    t = 0                   #Tiempo por el que va del episodio
+    imagenAnt = []
+    
+    while(True):
+        if reward != 0 or p.game_over():
+            if (reward == 1):
+                print(str(reward)+"Pasoooo")
+            else:
+                print(reward)
+            #Cada vez q termina un episodio
+            last = model.rewards[-1]
+            rewards = np.asarray(model.rewards)
+            rewards[rewards == 0] = last
+            model.rewards = rewards.tolist()
+            #print(model.rewards)
+            running_reward = running_reward * 0.99 + t * 0.01
+            finish_episode()
+            t = 0
+            if (p.game_over()):
+                p.reset_game()
+        observation = p.getScreenRGB()                   #Obtiene una nuev imagen del juego
+        new_image = convert_image(observation)    #Convierte la imagen a lo que recibe la red
 
-    for i in range(150):
-       if p.game_over():
-               p.reset_game()
-
-       observation = p.getScreenRGB()
-       new_image = convert_image(observation);
-       #cv.imwrite( "Imagenes/Gray_Image"+str(i)+".jpg", new_image );
-       action = 119
-       reward = p.act(action)
+        restaImg = []
+        res = 0
+        if imagenAnt != []:
+            restaImg = new_image-imagenAnt
+            res = select_action(model, restaImg)
+        imagenAnt = new_image
+            
+        #Selecciona la accion a realizar
+        #Descarga la imagen
+        #cv.imwrite( "Imagenes/Gray_Image"+str(i)+".jpg", new_image );
+        if (res == 1):
+            action = None
+        else:
+            action = 119
+           
+        reward = p.act(action)
+        #Guardo el reward
+        model.rewards.append(reward)
+        t = t+1
 
 run()
 
